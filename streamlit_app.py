6a7507005a8691da60d1d20f7c48a3405f7f4971
Sam Faar <77908823+samfaar@users.noreply.github.com>
2022-03-28 11:01:18 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,7 +83,7 @@ with news_headline_SA:
                       height=520, 
                       legend=dict(orientation="h",
                                   yanchor="top",
-                                  y=1.2,
+                                  y=1.22,
                                   xanchor="center",
                                   x=0.48))
     st.plotly_chart(fig)
